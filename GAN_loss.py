1cbe9c1d016f7909a38bb1f07c23b06246b4a56b
Christian Metz <cm@agrirobot.ai>
2022-06-23 07:37:11 +0200

    GAN_LOSS

--- GAN_loss.py
+++ GAN_loss.py
@@ -2,8 +2,11 @@ import torch
 
 def discriminator_loss(loss_fn, prediction_real, prediction_fake):
     """
-    ! does not return the same as we expect from the question 3
+    Fixed to solve question 5. Needs sigmoid.
     """
+    
+    prediction_fake = torch.sigmoid(prediction_fake)
+    prediction_real = torch.sigmoid(prediction_real)
     
     real_labels = torch.ones(prediction_real.size(0))
     fake_labels = torch.zeros(prediction_fake.size(0))
@@ -11,15 +14,31 @@ def discriminator_loss(loss_fn, prediction_real, prediction_fake):
     error_real = loss_fn(prediction_real, real_labels)
 
     error_fake = loss_fn(prediction_fake, fake_labels)
+    
     print('error real:', error_real )
     print('fake real:', error_fake)
     
     total_error = error_real + error_fake
     return total_error
 
+
+def discriminator_loss_v2(prediction_real, prediction_fake):
+    """
+    Fixed to solve question 5. Applies minmax loss
+    """
+        
+    real_labels = torch.ones(prediction_real.size(0))
+    fake_labels = torch.zeros(prediction_fake.size(0))
+    
+    sigmoid = lambda x: 1/(1+torch.exp(-x))
+
+    loss = -(torch.mean(torch.log(sigmoid(prediction_real))) + torch.mean(torch.log(1 - sigmoid(prediction_fake))))
+
+    return loss
+
 # TODO: Wasserstein loss
 
-
+ 
 
 
 #-------------------------------------------------
@@ -29,4 +48,6 @@ prediction_fake = torch.tensor([0.5,0.1])
 
 loss_fn = torch.nn.BCELoss()
 
-print(discriminator_loss(loss_fn, prediction_real, prediction_fake))
+print('V1:', discriminator_loss(loss_fn, prediction_real, prediction_fake))
+
+print('V2:', discriminator_loss_v2(prediction_real, prediction_fake))
